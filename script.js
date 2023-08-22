42d0ea09d4e0b4c3f13165f1bffd7a93d3d386dc
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-08-22 12:53:38 +0200

    Nu virker det med respons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const knap = document.getElementById("knap");
+const state = document.querySelector(".status");
 
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -7,13 +8,13 @@ let number = generateRandomNumber(1, 100);
 knap.addEventListener("click", () => {
   let guess = document.getElementById("guess").value;
   console.log(number);
-  if (guess === number) {
-    console.log("true");
+  if (guess == number) {
+    state.textContent = "JA, det var nemlig " + number;
   }
   if (guess > number) {
-    console.log("for højt");
+    state.textContent = "Dit gæt er for højt :((((";
   }
   if (guess < number) {
-    console.log("for lavt");
+    state.textContent = "Dit gæt er for lavt :((((";
   }
 });
