902ded3c81fb5ea3bcb01ea6d20fcfeeefd9ee4e
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-08-25 09:47:36 +0200

    ændret script

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 "use strict";
 
-const knap = document.getElementById("knap");
-const final = document.querySelector(".final");
+// const knap = document.getElementById("knap");
 
-function momsberegner(number, moms = 25) {
-  return number * (moms / 100 + 1);
+// const final = document.querySelector(".final");
+
+// knap.addEventListener("click", () => {
+//   const belob = document.getElementById("guess").value;
+
+//   final.textContent = momsberegner(belob);
+// });
+
+// function momsberegner(number, moms = 25) {
+//   return number * (moms / 100 + 1);
+// }
+
+// const result = greeting("Klaus");
+const txt = ` Halløj ${greeting("Klaus")}`;
+// console.log(result);
+
+function greeting(firstName) {
+  return `Hello ${firstName}`;
 }
-
-knap.addEventListener("click", () => {
-  final.textContent = momsberegner(document.getElementById("guess").value);
-});
