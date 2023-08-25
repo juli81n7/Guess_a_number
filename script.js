05fbb2f41c6b6e3cdc90525b384ffb7b125af717
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-08-25 09:53:56 +0200

    lavet det

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const output = document.querySelector(".number");
 const knap = document.getElementById("knap");
-let min = document.querySelector(".min").value;
-let max = document.querySelector(".max").value;
-
-function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
 knap.addEventListener("click", () => {
-  let min = document.querySelector(".min").value;
-  let max = document.querySelector(".max").value;
-  let number = generateRandomNumber(min, max);
-  output.textContent = number;
-  console.log(min);
-  console.log(max);
+  const h = document.querySelector(".min").value;
+  const b = document.querySelector(".max").value;
+  output.textContent = udregn(h, b);
+  console.log(udregn(h, b));
 });
+
+function udregn(højde, bredde) {
+  return højde * bredde;
+}
