43ca343d8531079053a5a0471c491de2f37a4798
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-08-25 10:10:24 +0200

    Nu omregner den også til square feer

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ knap.addEventListener("click", () => {
   const h = document.querySelector(".min").value;
   const b = document.querySelector(".max").value;
   output.textContent = udregn(h, b);
-  console.log(udregn(h, b));
+  console.log(convertSquareMToSquareFt(udregn(h, b)));
 });
 
 function udregn(højde, bredde) {
   return højde * bredde;
+}
+
+function convertSquareMToSquareFt(areal) {
+  return areal * 10.7639104;
 }
