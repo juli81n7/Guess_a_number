b03462421156c69578dbfc69d9a2dc5ded0e33ff
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-08-28 15:27:00 +0200

    lavet det men higher knappen virker ikke

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const output = document.querySelector(".number");
 const knap = document.getElementById("knap");
-let min = document.querySelector(".min").value;
-let max = document.querySelector(".max").value;
+
+const low = document.getElementById("low");
+const high = document.getElementById("high");
+const sandt = document.getElementById("sandt");
+
+let tal = document.querySelector(".tal").value;
 
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 knap.addEventListener("click", () => {
-  let min = document.querySelector(".min").value;
-  let max = document.querySelector(".max").value;
-  let number = generateRandomNumber(min, max);
+  let number = generateRandomNumber(0, 100);
   output.textContent = number;
-  console.log(min);
-  console.log(max);
+});
+
+low.addEventListener("click", () => {
+  let first = document.querySelector(".number").textContent;
+  let output = document.querySelector(".number");
+  let number = generateRandomNumber(0, first);
+  output.textContent = number;
+  console.log(number);
+});
+
+high.addEventListener("click", () => {
+  let first = document.querySelector(".number").textContent;
+  let output = document.querySelector(".number");
+  let number = generateRandomNumber(first, 100);
+  output.textContent = number;
+  console.log(number);
+});
+
+sandt.addEventListener("click", () => {
+  let output = document.querySelector(".number");
+
+  output.textContent = "YES - I KNEW IT";
 });
